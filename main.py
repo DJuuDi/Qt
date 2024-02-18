f707fd75f3f1e4106f0d425f4ad2ebe99a9e669d
DJuuDi <152791461+DJuuDi@users.noreply.github.com>
2024-02-18 17:34:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+import json
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
-import secrets 
-class PassGenerator(QMainWindow):
+
+class Widget(QMainWindow):
     def   __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.generate_password.clicked.connect(self.generet_password)
+        self.notes = {
+            "Замітка(гугл)":{
+                "текст": "Мій пароль від гугл_ак: 5345julia5345",
+                "теги": []
+            }
+        }
+        self.ui.noet_list.addItems(self.notes)
+        self.ui.noet_list.itemClicked.connect(self.show_note)
+        self.ui.save_btn.clicked.connect(self.save_note)
+    def show_note(self):
+        name = self.ui.noet_list.selectedItems()[0].text()
+        self.ui.title_edit.setText(name)
+        self.ui.text_edit.setText(self.notes[name]["текст"])
 
-    def generet_password(self):
-        password = secrets.token_hex(8)
-        self.ui.password_labl.setText(password)
-        with open("my_pass.txt", "w") as file:
-            file.write(password)
+    def save_note(self):
+        self.notes[self.ui.title_edit.text()] = self.ui.title_edit.text()
+        with open("notes.json", "w", encoding="utf-8") as file:
+            json.dump(self.notes, file)
+
 
 
 app = QApplication([])
-ex = PassGenerator()
+ex = Widget()
 ex.show()
 app.exec_()
